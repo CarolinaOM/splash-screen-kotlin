c36aa4c1ecfc4835e624c85c6d8abde1093ed79f
CarolinaOM <caro13081991@gmail.com>
2022-01-20 15:14:12 +0000

    Agregando proyecto

---
+++ app/src/main/java/com/example/mysplashscreen/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.mysplashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        Thread.sleep(2000)
+
+        setTheme(R.style.AppTheme)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
